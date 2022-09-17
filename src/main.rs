8aaf93f7158a7c401aaa8576fd54eb0a768952c0
medzernik <medzernik1@gmail.com>
2022-09-17 23:16:20 +0200

    started working on saliny.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use crate::saliny::begin_saliny;
+
 mod cache;
 mod pseudo_database;
+mod saliny;
 
 fn main() {
     //cache::cache_booger();
-    pseudo_database::start_database_interaction();
+    //pseudo_database::start_database_interaction();
+    begin_saliny();
 }
 
 // UwU <3
---
+++ src/saliny.rs
@@ -0,0 +1,7 @@
+pub fn begin_saliny(){
+
+
+}
+
+
+
