ecbb17510fcff39388f5bdda5f37cd68f643ebd8
medzernik <medzernik1@gmail.com>
2022-09-17 23:57:09 +0200

    started working on saliny.rs

--- src/saliny.rs
+++ src/saliny.rs
@@ -1,7 +1,58 @@
-pub fn begin_saliny(){
+use chrono::prelude::*;
+use chrono::{Date, DateTime, Utc};
 
-
+struct TramStation {
+    station_name: String,
+    timetables: Vec<TimeTable>,
+    train_access: bool,
 }
 
+impl PartialEq for TramStation {
+    fn eq(&self, other: &Self) -> bool {
+        self.station_name == other.station_name
+    }
+}
 
+struct TramLine {
+    line_number: u16,
+    line_name: String,
+    stations: Vec<TramStation>,
+}
+
+struct TimeTable {
+    timetable_station: TramStation,
+    timetable_line: TramLine,
+    timetable_time: Vec<DateTime<Utc>>,
+}
+
+pub fn begin_saliny() {
+    let mut tram_stations: Vec<TramStation> = Vec::new();
+    let mut tram_line: Vec<TramLine> = Vec::new();
+}
+
+fn show_stations(line: &TramLine) {
+    line.stations
+        .iter()
+        .for_each(|station| println!("{}", station.station_name))
+}
+
+fn is_station_on_line(line: &TramLine, station: &TramStation) -> bool {
+    line.stations.contains(station)
+}
+
+fn list_common_stations(line1: &TramLine, line2: &TramLine) {
+    line1.stations.iter().for_each(|station| {
+        if is_station_on_line(line2, station) {
+            println!("{}", station.station_name);
+        }
+    });
+}
+
+fn list_stations_with_train(line1: &TramLine) {
+    line1.stations.iter().for_each(|station| {
+        if station.train_access == true {
+            println!("{}", station.station_name)
+        }
+    })
+}
 
